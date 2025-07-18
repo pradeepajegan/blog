c15e09008e1b7fcb5037ec3d7d7a6124acbc8096
pradeepajegan <pradeepa.m2023cce@sece.ac.in>
2025-07-18 09:15:08 +0530

    updated

--- app/(user)/blog/[blogId]/page.js
+++ app/(user)/blog/[blogId]/page.js
@@ -1,4 +1,19 @@
-const BlogId = ({ params }) => {
+// const BlogId = ({ params }) => {
+//   const id = params.blogId;
+
+//   return (
+//     <div className="detail-container">
+//       <img src={`/image${id}.jpg`} className="detail-image" />
+//       <p>This is the description for Blog {id}.</p>
+//     </div>
+//   );
+// };
+// export default BlogId;
+"use client";
+import { useParams } from "next/navigation";
+
+const BlogId = () => {
+  const params = useParams();
   const id = params.blogId;
 
   return (
@@ -8,4 +23,5 @@ const BlogId = ({ params }) => {
     </div>
   );
 };
+
 export default BlogId;
